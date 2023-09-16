ad7782f53421f89cc61649f5b70aea2778ce578e
J0sh2210 <132153959+J0sh2210@users.noreply.github.com>
2023-09-16 11:53:45 -0600

    Add files via upload

---
+++ ejercicio1 parcial 2.py
@@ -0,0 +1,10 @@
+palabra = input("Ingrese una palabra: ")
+
+mayus = "A"
+minus = "a"
+
+cant_mayus = palabra.count(mayus)
+cant_minus = palabra.count(minus)
+
+print (f"Hay '{cant_mayus}' 'A' mayusculas y '{cant_minus}' 'a' minusculas")
+
---
+++ ejercicio2 parcial2.py
@@ -0,0 +1,19 @@
+cuadrado = float(input("ingre un número que desee el cuadrado: "))
+
+cuadrado = cuadrado ** 2
+
+print (f"el cuadrado es {cuadrado}")
+
+print (" ")
+print (" ")
+print (" ")
+
+print ("Calculadora de producto de dos números")
+
+
+num_uno = int(input("ingrese su primer número: "))
+num_dos = int(input("ingrese su segundo número: "))
+
+producto = num_uno * num_dos
+
+print (f"el producto es {producto}")
